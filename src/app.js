5f630464f3134e0b0c008de5eb4fbeed369eafef
mgtablante <mgtablantel@gmail.com>
2024-08-09 17:46:10 +0000

    Ejercicio excusas listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["Amiga", "Pajaro", "Hamster", "Tia"];
+let action = ["Comido", "cocino", "Regalado", "accionado"];
+let what = ["mesa", "perro", "silla"];
+let when = ["Clinica", "hospital", "casa", "carro"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let index = Math.floor(Math.random() * 3);
+  document.querySelector("#excuse").innerHTML =
+    who[index] +
+    " " +
+    action[index] +
+    " " +
+    what[index] +
+    " " +
+    when[index] +
+    " "; //write your code here
+  console.log("");
 };
